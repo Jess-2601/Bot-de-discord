feda2d21a464c535396bcd43a7f8d72df6f864f4
Mafer Quintero <mfquintero@estud.usfq.edu.ec>
2026-03-10 13:32:34 -0500

    fix comando registrar

--- deploy-commands.js
+++ deploy-commands.js
@@ -48,7 +48,10 @@ try {
 console.log("🔄 Registrando comandos...")
 
 await rest.put(
-Routes.applicationCommands(process.env.CLIENT_ID),
+Routes.applicationGuildCommands(
+process.env.CLIENT_ID,
+process.env.GUILD_ID
+),
 { body: commands }
 )
 
